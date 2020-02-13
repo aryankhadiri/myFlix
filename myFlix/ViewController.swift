0aac937d3bf1c2090463972a5b515eb60ba1a2f4
Aryan Khadiri <aryankhadiri@gmail.com>
2020-02-12 16:19:40 -0800

    Initial Commit

---
+++ myFlix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myFlix
+//
+//  Created by Aryan Khadiri on 2/12/20.
+//  Copyright © 2020 AryanKhadiri@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
